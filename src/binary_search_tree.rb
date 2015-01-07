a2bff1e3ec029eb4f3a94bc9cfc0e636ecf8d70d
Christopher Lam <chrislam67@gmail.com>
2015-01-06 21:17:53 -0600

    fixed insert, and added depth

--- src/binary_search_tree.rb
+++ src/binary_search_tree.rb
@@ -10,13 +10,21 @@ class Node
 	def insert(value)
 		if value == self.value
 			puts "same value do nothing, we don't insert duplicates"
-			elsif value < self.value
-				puts "value to insert is less than node value"
+		elsif value < self.value
+			puts "value to insert is less than node value"
+			if left.nil?
 				self.left = Node.new(value)
-			elsif value > self.value
-				puts "value to insert is greater than node value"
-				self.right = Node.new(value)
+			elsif
+				self.left.insert(value)
 			end
+		elsif value > self.value
+			puts "value to insert is greater than node value"
+			if right.nil?
+				self.right = Node.new(value)
+			elsif
+				self.right.insert(value)
+			end
+		end
 	end
 
 	def printInOrder
@@ -43,15 +51,33 @@ class BinarySearchTree
 		end
 	end
 
+	def depth
+		depthOfNode(self.root)	
+	end
+
+	def depthOfNode(node)
+		if node.nil? 
+			return 0
+		end
+
+		return [depthOfNode(node.left), depthOfNode(node.right)].max + 1
+	end
+
 	def printInOrder
 		self.root.printInOrder
 	end
+
+
 end
 
 bst = BinarySearchTree.new
 bst.insert(5)
 bst.insert(15)
+bst.insert(2)
+bst.insert(25)
 
 puts bst.root.value
 puts "-----------------"
 bst.printInOrder
+puts "-----------------"
+puts bst.depth
